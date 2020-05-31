00a45cce2d13b1a39d6b9e760782bed9cbf5b56f
wanion1106 <wanion.kanchi@icloud.com>
2020-05-31 13:47:05 +0900

    ヘッダーにユーザー写真、編集ボタン追加

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
 
 class HomeController extends Controller
 {
@@ -21,8 +22,10 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
+    public function index(User $user)
     {
-        return view('home');
+        return view('home',[
+            'user' => $user,
+        ]);
     }
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,7 +25,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    筋トレ日記
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -52,6 +52,12 @@
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    @if(Auth::user()->avatar_img == null)
+                                    <img src="/images/no_image.png" width="40">
+                                    @else
+                                    <img src="/storage/avatars/{{ Auth::user()->avatar_img }}" width="40">
+                                    @endif
+
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
 
@@ -61,10 +67,15 @@
                                                      document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
+                                    
 
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                         @csrf
                                     </form>
+
+                                    <a class="dropdown-item" href="#">
+                                        ユーザー編集
+                                    </a>
                                 </div>
                             </li>
                         @endguest
