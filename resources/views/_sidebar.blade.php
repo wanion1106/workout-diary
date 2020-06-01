9bd037df8e0b455e81219a3b01b989f4e21fe101
wanion1106 <wanion.kanchi@icloud.com>
2020-06-02 07:26:40 +0900

    サイドバーを追加

---
+++ resources/views/_sidebar.blade.php
@@ -0,0 +1,17 @@
+<ul>
+    <li class="mb-2">
+        <a href="/">TOP</a>
+    </li>
+
+    <li class="mb-2">
+        <a href="#">Timeline</a>
+    </li> 
+
+    <li class="mb-2">
+        <a href="#">MyDiary</a>
+    </li>
+
+    <li class="mb-2">
+        <a href="#">Info</a>
+    </li>
+</ul>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-    dashboard
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-2">
+                @include('_sidebar')
+            </div>
+            <div class="col-sm-10">
+                dashboard
+            </div>
+        </div>
+    </div>
 @endsection
