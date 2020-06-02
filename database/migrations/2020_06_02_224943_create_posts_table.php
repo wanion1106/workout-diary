e10e4a8c3b1d4fd0486917cda9502f429e7dc8fd
wanion1106 <wanion.kanchi@icloud.com>
2020-06-03 08:00:03 +0900

    migrate内容作成

--- database/migrations/2020_06_02_224943_create_posts_table.php
+++ database/migrations/2020_06_02_224943_create_posts_table.php
@@ -15,7 +15,10 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->string('body');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
