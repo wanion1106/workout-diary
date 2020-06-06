418f6f124c8be7d25bbe3ce384fe0eae7cb2bae2
wanion1106 <wanion.kanchi@icloud.com>
2020-06-06 16:15:00 +0900

    ツイートフォームのレイアウト変更

--- resources/views/_tweet-form.blade.php
+++ resources/views/_tweet-form.blade.php
@@ -1,21 +1,7 @@
 <div class="border border-primary p-2 mb-4">
     <form method="POST" action="{{ route('home.store') }}" enctype="multipart/form-data">
         @csrf
-        <textarea name="body" class="form-control"></textarea>
-
-        <hr class="mt-0">
-
-        <div class="d-flex justify-content-between">
-        
-            @if(Auth::user()->avatar_img == null)
-                <img src="/images/no_image.png" width="40" class="rounded-circle">
-            @else
-                <img src="/storage/avatars/{{ Auth::user()->avatar_img }}" width="40" class="rounded-circle">
-            @endif
-
-            <input type="file" name="photo_img">
-
-            @if ($errors->any())
+        @if ($errors->any())
             <div class="alert alert-danger">
                 <ul>
                 @foreach ($errors->all() as $error)
@@ -23,10 +9,31 @@
                 @endforeach
                 </ul>
             </div>
-            @endif
+        @endif
 
+        <textarea name="body" class="form-control"></textarea>
 
-            <button type="submit" class="btn btn-primary shadow">投稿する</button>
+        <hr class="mt-0">
+
+        <div class="d-flex">
+            <div class="mr-auto">
+                @if(Auth::user()->avatar_img == null)
+                    <img src="/images/no_image.png" width="40" class="rounded-circle">
+                @else
+                    <img src="/storage/avatars/{{ Auth::user()->avatar_img }}" width="40" class="rounded-circle">
+                @endif
+            </div>
+
+            <div class="mr-2">
+                <label for="photo_img" class="btn btn-secondary">
+                    +写真を追加
+                    <input type="file" name="photo_img" id="photo_img" class="d-none">
+                </label>
+            </div>
+
+            <div>
+                <button type="submit" class="btn btn-primary shadow">投稿する</button>
+            </div>
 
         </div>
     </form>
